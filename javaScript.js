c1fbf47de1babe284f545bbe6b9a669371416378
Dawid Zabielski <D.G.Zabielski@gmail.com>
2021-06-01 17:30:20 +0200

    Upload first purified code

--- javaScript.js
+++ javaScript.js
@@ -2,7 +2,8 @@ var allCards = document.querySelectorAll('.card');                              
 
 function shuffle(element)                                                                              //apply fisher's randomizer.
 {      
-    for (var i = 0; i < element.length-1; i++){
+    for (var i = 0; i < element.length-1; i++)
+    {
         var j = i + Math.random()*(element.length - 1 - i);
         j = Math.round(j);
         var x = element[j];
@@ -14,38 +15,37 @@ function shuffle(element)                                                       
 
 window.onload = function()                                                                             //function which uses fisher's randomizer, to shuffle the cards. function will start on load of a webside.
 {
-    let cardsList = Array.from(allCards)
-    var shuffleTheCard = shuffle (cardsList)    
-    console.log(shuffleTheCard)
+    let cardsList = Array.from(allCards);
+    var shuffleTheCard = shuffle (cardsList);
+    console.log(shuffleTheCard);
     var anyCard = document.querySelector('.table');
     anyCard.innerHTML = '';
 
-        for (var i = 0; i<shuffleTheCard.length; i++)
-        {
-        anyCard.appendChild(shuffleTheCard[i])
-        }
+    for (var i = 0; i<shuffleTheCard.length; i++)
+    {
+    anyCard.appendChild(shuffleTheCard[i])
+    };
 }; 
 
 
-for(const a1 of allCards)                                                                              //create listener for event "click".
+for(const a1 of allCards)                                                                               //create listener for event "click".
 {
 	a1.addEventListener('click', revealCard);		
 };
                     
-var turnCounter = 0;                                                                                   //create some variables.
+var turnCounter = 0;                                                                                    //create some variables.
 var revealed = [];
 var pairsLeft = 8;
 
-function revealCard()                                                                                  //function "revealCard" only reveales card.
+function revealCard()                                                                                   //function "revealCard" only reveales card.
 {
-    if (revealed.length > 1 || revealed.indexOf(this) != -1 || $(this).hasClass("matched")) return     //if there are more than two cards revealed, stop function.
-    $(this).addClass("cRevealed");                                                                     //adds class reveald.
-    revealed.push(this)                                                                                //adds number of revealed cards.
-
-    if (revealed.length == 2) compareCards()                                                           //if there are 2 cards revealed, start function "compareCards".
+    if (revealed.length > 1 || revealed.indexOf(this) != -1 || $(this).hasClass("matched")) return;     //if there are more than two cards revealed, stop function.
+    $(this).addClass("cRevealed");                                                                      //adds class reveald.
+    revealed.push(this);                                                                                //adds number of revealed cards.
+    if (revealed.length == 2) compareCards();                                                           //if there are 2 cards revealed, start function "compareCards".
 };
                        
-function hideCards(b2)                                                                                 //delete added class of revealed card.
+function hideCards(b2)                                                                                  //delete added class of revealed card.
 {
     $(b2).removeClass("cRevealed");
 };
@@ -58,18 +58,19 @@ function compareCards()
     var cardAlpha = revealed[0];                                                                       //make variable for two marked cards.
     var cardBeta = revealed[1];
                                                         
-        if (cardAlpha.dataset.avatar == cardBeta.dataset.avatar)                                       //if first card has same Data as second card.
-        {
-            correct(cardAlpha, cardBeta)                                                               //run function "correct".
-            revealed = []
-        }   
+    if (cardAlpha.dataset.avatar == cardBeta.dataset.avatar)                                           //if first card has same Data as second card.
+    {
+        correct(cardAlpha, cardBeta)                                                                   //run function "correct".
+        revealed = []
+    }   
 
-        else                                                                                           //else use function hideCards for cardAlpha and cardBeta.
+    else                                                                                               //else use function hideCards for cardAlpha and cardBeta.
+    {
+        setTimeout(function()
         {
-            setTimeout(function(){
-            hideCards(cardAlpha); 
-            hideCards(cardBeta);
-            revealed = []   
+        hideCards(cardAlpha); 
+        hideCards(cardBeta);
+        revealed = []   
         }, 1000);                                                                                      //one second time, before the hide action.
     }                                                     
 };                                                                                                     //for both ways, clear list "revealed".
@@ -78,9 +79,8 @@ function correct(c1, c2)                                                        
 {
     $([c1, c2]).addClass ("matched");
     pairsLeft-- ;                                                                                      //delete one count of pairs left.
-
-        if (pairsLeft == 0)                                                                            //when pairsLeft hit 0, change element table to <h1> text.
-        {
-            $(".table").html("<h1>You win!!!<br>You did it in: "+turnCounter+" turns</h1>");
-        }
+    if (pairsLeft == 0)                                                                                //when pairsLeft hit 0, change element table to <h1> text.
+    {
+        $(".table").html("<h1>You win!!!<br>You did it in: "+turnCounter+" turns</h1>");
+    };
 };
